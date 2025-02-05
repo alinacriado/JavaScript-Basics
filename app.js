57bee2e80e287e1e34ec499f1fae7402ee671ce2
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-05 18:48:14 -0500

    Арифметические операторы

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello, World');
+const width = 10;
+const hight = 5;
+const space = 10 * 5;
+const newSpace = width * hight;
+const newWidth = width + 3;
+const division = width / newWidth;
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(space);
+
+//Строки
+
+const city = 'Moscow';
+const street = 'Kerveskaya';
+console.log(city + ', ' + street); // сложение строк - конкатенация
+console.log(city + ', ' + street + ' ' + 5); // 5 становится строкой
