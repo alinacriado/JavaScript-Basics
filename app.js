4b5a8d8ab2cf671c2021a07b77e8f588f23bdcd8
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-07 15:47:55 -0500

    анонимные функции

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-// function logName() {
-//   console.log('Моё имя Антон');
-// }
-
-// logName();
-
-function logName(name, surname) {
-  console.log(`Моё имя ${name} ${surname}`);
+function powerOfTwo(number) {
+  return number * number;
 }
+console.log(powerOfTwo(5)); // можно также вызвать функцию перед её объявлением
 
-logName('Alina', 'Bakhvalova');
-
-function countDepositSum(depositInUSD, month, rateInPercent) {
-  const sum = depositInUSD * (1 + rateInPercent / 100 / 12) ** month;
-  return sum;
-
-  //или
-  //return depositInUSD * (1 + rateInPercent / 100 / 12) ** month;
+const poft = function (number) {
+  return number * number;
 }
-
-const example1 = countDepositSum(2000, 24, 7);
-console.log(example1);
-
-const example2 = countDepositSum(1000, 48, 10);
-console.log(example2);
-
-console.log(countDepositSum(1000, 24, 12));
+console.log(poft(8));
