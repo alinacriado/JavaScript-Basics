d9b260ff95999b506c2e76a0cf72e364a7e3d634
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-08 17:57:00 -0500

    slice, splice, reverse, concat

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const roles = ['user', 'admin', 'manager'];
+const roles = ['user', 'admin', 'manager', 'superuser'];
 
-const elIndex = roles.indexOf('admin');
-console.log(elIndex);
+const res = roles.slice(2); // с элемента с индексом 2 и до конца
+console.log(res); // ['manager', 'superuser']
+console.log(roles); // ['user', 'admin', 'manager', 'superuser'] - slice не меняет массив
 
-const elIndex2 = roles.indexOf('superuser');
-console.log(elIndex2); // -1 = не найден
+const res2 = roles.slice(2, 3); // с элемента с индексом 2 до элемента с индексом 3 (элемент с конечным индексом не включён)
+console.log(res2); // ['manager']
 
-console.log(roles.includes('admin'));
+const res3 = roles.slice(-1); // последний элемент
+const res4 = roles.slice(-2); // 2 последних элемента
 
-if (roles.includes('admin')) {
-	console.log('доступ есть');
-}
+const res5 = roles.splice(2);
+console.log(res5); // ['manager', 'superuser']
+console.log(roles); // ['user', 'admin'] - splice отрезает кусок от начального массива
+
+const res6 = roles.splice(2, 3); // // с элемента с индексом 2 берет 3 элемента
+
+const res7 = roles.slice(-1); // отрежет последний элемент
+
+roles.reverse(); // переворачивает массив
+
+const newRoles = ['sysadmin', 'developer'];
+roles.concat(newRoles); // соединяет массивы
