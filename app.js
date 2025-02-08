57210aaf7c55c8688d2899b85d265098af65a181
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-08 16:29:06 -0500

    Массивы

--- app.js
+++ app.js
@@ -1,59 +1,15 @@
-/*
-	Пользователь:
-	- Возраст
-	- Наличие работы
-	- Деньги
-	Нужно проверить может ли он купить новый MacBook за 2000$?
-	Он может брать не только свои деньги, но и взять кредит.
-	Ему дадут 500$, только если ему больше 24-х лет и он
-	имеет работу, 100$ если ему просто больше 24-х лет и 0 в
-	ином случае.
-	Напишите функцию, которая принимает данные пользователя
-	и товара и возвращает true или false;
-*/
+const role1 = 'admin';
+const role2 = 'user';
+const role3 = 'superuser';
 
-const MacBookPrice = 2000;
-const deposit = 1000;
+const roles = ['admin', 'user', 'superuser'];
+const userInfo = ['Anna', 25];
 
-/* function computeCreditSum(age, haveJob) {
-	if (age > 24 && haveJob) {
-		return 500;
-	} else if (age > 24) {
-		return 100;
-	} return 0;
-}
+console.log(roles);
+console.log(roles[0]);
+console.log(roles.length);
+console.log(roles[roles.length - 1]);
 
-function canBuy(age, haveJob, deposit, productPrice) {
-	const userMoney = computeCreditSum(age, haveJob) + deposit;
-	console.log(`У вас ${userMoney}$`);
+console.log(roles.at(0));
 
-	if (userMoney >= productPrice) {
-		return true;
-	} else {
-		return false;
-	}
-} */
-
-function computeCreditSum(age, haveJob) {
-	return age > 24 && haveJob ? 500 : age > 24 ? 100 : 0;
-}
-
-function canBuy(age, haveJob, deposit, productPrice) {
-	const userMoney = computeCreditSum(age, haveJob) + deposit;
-	console.log(`У вас ${userMoney}$`);
-
-	return userMoney >= productPrice;
-}
-
-console.log(canBuy(23, true, 1000, MacBookPrice));
-
-/* function computeCreditSum(age, haveJob) {
-	switch(true) {
-		case age > 24 && haveJob:
-			return 500;
-		case age > 24:
-			return 100;
-		default:
-			return 0;
-	}
-} */
+const userNames = new Array('Vasiya', 'Petiya', 'Masha');
