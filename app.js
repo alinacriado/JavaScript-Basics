db7fe85ebac49ef682d9dfdbbf25d585e9a8d038
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-08 18:48:24 -0500

    Упражнение - Обновление списка задач

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+const tasks = ['Задача 1'];
 
-const url = 'auth/user/login';
-const res = url.split('/'); // возвращает массив
-console.log(res); // ['auth', 'user', 'login']
+const addTask = (task) => {
+  tasks.push(task);
+}
 
-console.log(roles.join('-')); // user-admin-manager-superuser, возвращает строку
+const removeTaskByName = (taskName) => {
+  if (tasks.includes(taskName)) {
+    const taskIndex = tasks.indexOf(taskName);
+    tasks.splice(taskIndex, 1);
+  } else {
+    return;
+  }
+}
+
+const doPriority = (taskName) => {
+  if (tasks.includes(taskName)) {
+    const taskIndex = tasks.indexOf(taskName);
+    tasks.splice(taskIndex, 1);
+    tasks.unshift(taskName);
+  } else {
+    return;
+  }
+}
+
+addTask('Задача 2');
+addTask('Задача 3');
+addTask('Задача 4');
+console.log(tasks);
+removeTaskByName('Задача 3');
+console.log(tasks);
+doPriority('Задача 4');
+console.log(tasks);
