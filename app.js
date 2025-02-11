3cb5b3874a95a92a2ad682309cdd3bea9e4483f1
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-11 16:33:59 -0500

    Циклы for of, for in

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-/* for (let i = 1; i < 5; i++) {
-  console.log(`Вывод - ${i}`);
-}
-
-let i = 1;
-while (i < 5) {
-  console.log(`Вывод - ${i}`);
-  i++;
-} */
-
-const arr = [1, 4, 8, 7];
+const arr = [1, 4, 8, 7, 'hdnjn'];
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 5) {
-    break;
-  }
   console.log(arr[i]);
 }
 
-let i = 0;
-while (arr[i] <= 5 && i < arr.length) {
-  console.log(arr[i]);
-  i ++;
+for (let element of arr) { // for of итерируется по элементам массива
+  console.log(element);
 }
 
-let j = 0;
-do {
-  console.log(j);
-  j++;
-} while(j < 0) // всегда отработает хотябы раз
+for (let index in arr) { // for in итерируется по индексам массива
+  console.log(arr[index]);
+}
