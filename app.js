8518b31b9aaa7cbccb54ceecb63a482b093eaae0
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-10 21:52:02 -0500

    Упражнение - Цикл в обратном порядке

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
+const arr = ['!', 'JS', 'люблю', 'Я'];
+let arrayReversed = [];
 
-for (let i = 0; i < tasks.length; i++) {
-  console.log(tasks[i]);
+for (let i = arr.length - 1; i >= 0; i--) {
+  arrayReversed.push(arr[i]);
 }
-console.log('---------')
-for (let i = 0; i < tasks.length; i++) {
-  if (tasks[i] === 'Задача 2') {
-    continue; // проскакивает и переходит к следующему
-  }
-  console.log(tasks[i]); // Задача 1, Задача 3
-}
-console.log('---------')
-for (let i = 0; i < tasks.length; i++) {
-  if (tasks[i] === 'Задача 2') {
-    break; // заканчивает работу цикла
-  }
-  console.log(tasks[i]); // Задача 1
-}
+
+console.log(arrayReversed.join(' '));
