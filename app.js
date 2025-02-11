3ccf279fb40962cb9ec60f5d5a5b4c7067e3bfaf
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-11 18:49:45 -0500

    Упражнение - Стрелочные функции

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function power(pow) {
-  return function (num) {
-    return num ** pow;
-  }
-}
+// function power(pow) {
+//   return function (num) {
+//     return num ** pow;
+//   }
+// }
+const power = pow => num => num ** pow;
 
 const powerOfTwo = power(2); // powerOfTwo становится функцией возведения в степень 2
 console.log(powerOfTwo(6)); // 36
@@ -10,4 +11,4 @@ console.log(powerOfTwo(6)); // 36
 const powerOfThree = power(3);
 console.log(powerOfThree(2));
 
-console.log(power(4)(5)); // 5 ** 4
+console.log(power(4)(5)); // 625 (5 ** 4)
