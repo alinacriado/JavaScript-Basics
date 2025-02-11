8330094b0a865356cecd2db3260b667c75189a10
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-10 21:40:39 -0500

    Циклы: break и continue

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// console.log('Наш баланс 1$');
-// console.log('Наш баланс 2$');
-// console.log('Наш баланс 3$');
-// console.log('Наш баланс 4$');
-// console.log('Наш баланс 5$');
-// console.log('Наш баланс 6$');
-// console.log('Наш баланс 7$');
-// console.log('Наш баланс 8$');
-// console.log('Наш баланс 9$');
+const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
 
-for (let i = 1; i < 10; i++) {
-  console.log(`Наш баланс ${i}$`);
+for (let i = 0; i < tasks.length; i++) {
+  console.log(tasks[i]);
+}
+console.log('---------')
+for (let i = 0; i < tasks.length; i++) {
+  if (tasks[i] === 'Задача 2') {
+    continue; // проскакивает и переходит к следующему
+  }
+  console.log(tasks[i]); // Задача 1, Задача 3
+}
+console.log('---------')
+for (let i = 0; i < tasks.length; i++) {
+  if (tasks[i] === 'Задача 2') {
+    break; // заканчивает работу цикла
+  }
+  console.log(tasks[i]); // Задача 1
 }
