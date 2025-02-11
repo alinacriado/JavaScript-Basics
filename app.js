76790f890aa680b5dac5588bb03045b8fea5fa05
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-11 17:54:01 -0500

    Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
-const arr = [1, 4, 8, 7, 'hdnjn'];
+/*
+  Есть выгрузка операций пользователя
+  const operations = [1000, -700, 300, -500, 10000];
+  а так же начальный баланс в 100$
+  Необходимо сделать функции расчёта:
+  - Итогового баланса
+  - Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
+  - Расчёта среднего расхода и среднего дохода
+*/
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+const operations = [1000, -700, 300, -500, 10000];
+const initialBalance = 100;
+
+const getResult = (arrayOfOperations, startingBalance) => {
+  let result = 0;
+  for (const operation of arrayOfOperations) {
+    result += operation;
+  }
+  return startingBalance + result;
 }
 
-for (let element of arr) { // for of итерируется по элементам массива
-  console.log(element);
+const getNegativeBalance = (arrayOfOperations, startingBalance) => {
+  let balance = startingBalance;
+  let isPositive = true;
+  for (const operation of arrayOfOperations) {
+    balance += operation;
+    if (balance < 0) {
+      isPositive = false;
+      break;
+    }
+  }
+  return isPositive;
 }
 
-for (let index in arr) { // for in итерируется по индексам массива
-  console.log(arr[index]);
+const getAverage = (arrayOfOperations) => {
+  let incomes = 0;
+  let incomesNumber = 0;
+  let expenses = 0;
+  let expensesNumber = 0;
+
+  for (const operation of arrayOfOperations) {
+    if (operation > 0) {
+      incomes += operation;
+      incomesNumber ++;
+    }
+    if (operation < 0) {
+      expenses += -operation;
+      expensesNumber ++;
+    }
+  }
+  const averageIncome = incomes / incomesNumber;
+  const averageExpense = expenses / expensesNumber;
+
+  console.log(`Средний доход = ${averageIncome}$, cредний расход = ${averageExpense}$`);
 }
+
+console.log(`Итоговый баланс: ${getResult(operations,initialBalance)}$`);
+console.log(getNegativeBalance(operations, initialBalance));
+getAverage(operations);
