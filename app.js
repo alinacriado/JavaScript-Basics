745e12262378bf088ca93c9285e643654a9bbc40
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-12 10:48:33 -0500

    Упражнение - среднее значение

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const operations = [100, -20, 7, -30, 50];
-let balance = 0;
+const arr = [2, 4, 4, 10];
 
-for (const operation of operations) {
-  balance += operation;
-}
-
-const finalBalance = operations.reduce((acc, value) => {
-  return acc += value;
-}, 0);
-console.log(finalBalance);
-// 0 - acc = 0, value = 100
-// 1 - acc = 100, value = -20
-// 2 - acc = 80, value = 7 ...
-
-const minElement = operations.reduce((acc, operation) => {
-  if (operation > acc) {
-    return acc;
+const avg = arr.reduce((acc, num, i) => {
+  if (i != arr.length - 1) {
+    return acc + num;
   } else {
-    return operation;
+    return (acc + num) / arr.length;
   }
 }, 0);
-console.log(minElement);
-// 0 - acc = 0, operation = 100; 100 > 0 true; return acc
-// 1 - acc = 0, operation = -20; -20 > 0 false; return operation
-// 2 - acc = -20, operation = 7; ...
+
+console.log(avg);
