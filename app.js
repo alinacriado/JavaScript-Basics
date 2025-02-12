6059f7fca5bd03aebd20cbf0cfc0f72a539fd52a
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-11 20:24:51 -0500

    метод filter

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const transactionsInUSD = [10, -7, 50, -10, 100];
+const operations = [100, -20, 7, -10, 50];
 
-const transactionsInRUB = [];
-for (const transaction of transactionsInUSD) {
-  transactionsInRUB.push(transaction * 60);
-}
-console.log(transactionsInRUB); // [600, -420, 3000, -600, 6000]
+// const positiveOperations = [];
+// for (const operation of operations) {
+//   if (operation > 0) {
+//     positiveOperations.push(operation);
+//   }
+// }
+// console.log(positiveOperations); // [100, 7, 50]
 
-const transactionsInRUB2 = transactionsInUSD.map((transaction) => transaction * 60); // map возвращает новый массив
+const positiveOperations = operations.filter(operation => {
+  return operation > 0;
+});
+console.log(positiveOperations); // [100, 7, 50]
 
-console.log(transactionsInRUB2); // [600, -420, 3000, -600, 6000]
+const positiveRUBOperations = operations
+  .filter(operation => operation > 0)
+  .map(operation => operation * 60);
+  console.log(positiveRUBOperations);
