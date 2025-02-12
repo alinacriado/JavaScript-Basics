b44dda73efa62b789c5cbbbad6552dd5be37986c
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-12 15:38:14 -0500

    find, findIndex

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const arr = [2, 4, 4, 10];
+const arr = [2, 4, 4, 10, 20];
 
-const avg = arr.reduce((acc, num, i) => {
-  if (i != arr.length - 1) {
-    return acc + num;
-  } else {
-    return (acc + num) / arr.length;
-  }
-}, 0);
+let elGT5;
+// for (const el of arr) {
+//   if (el > 5) {
+//     elGT5 = el;
+//     break;
+//   }
+// }
 
-console.log(avg);
+elGT5 = arr.find(el => el > 5); //найдёт первый элемент подходящий под условие
+elGT5Index = arr.findIndex(el => el > 5); // не нашел: вернет -1
+
+console.log(elGT5); // 10
+console.log(elGT5Index); // 3
