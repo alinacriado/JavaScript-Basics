7c6644c3209235ffca3516329dd049665245fd46
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-14 17:07:22 -0500

    Базовые методы строк

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const arr = [1, 2, 3, 4, 5];
-console.log(new Array(1, 2, 3, 4, 5));
+const userName = 'Вася Пупкина';
+console.log(userName[0]); // В
+console.log(userName.charAt(0)); // В
 
-const arr2 = new Array(5); // [empty × 5]
-arr2.fill(1); // [1, 1, 1, 1, 1]
+console.log(userName.length); // 12
 
-const arr3 = new Array(5);
-arr3.fill(1, 0, 3);
-console.log(arr3); // [1, 1, 1, empty × 2]
-arr3.fill(2, 3, 5);
-console.log(arr3); // [1, 1, 1, 2, 2]
+console.log(userName.indexOf('а')); // 1
+console.log(userName.lastIndexOf('а')); // 11
+console.log(userName.indexOf('уп')); // 6
+console.log(userName.indexOf('упв')); // -1
 
-// const arr4 = Array.from({ length: 5 }, () => 5);
-// console.log(arr4); // [5, 5, 5, 5, 5]
+console.log(userName.includes('у')); // true
 
-const arr4 = Array.from({ length: 5 }, (cur, i) => i + 1);
-console.log(arr4); // [1, 2, 3, 4, 5]
+console.log(userName.slice(5)); // Пупкина
+console.log(userName.slice(5, 8)); // Пуп
