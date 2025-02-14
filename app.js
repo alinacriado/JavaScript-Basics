eb2f1013a1e41e50b64b5cae2bfd7ab1ccf95d56
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-14 17:29:16 -0500

    Упражнение - выделение имени

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const userName = 'Вася Пупкина';
-console.log(userName[0]); // В
-console.log(userName.charAt(0)); // В
-
-console.log(userName.length); // 12
-
-console.log(userName.indexOf('а')); // 1
-console.log(userName.lastIndexOf('а')); // 11
-console.log(userName.indexOf('уп')); // 6
-console.log(userName.indexOf('упв')); // -1
-
-console.log(userName.includes('у')); // true
-
-console.log(userName.slice(5)); // Пупкина
-console.log(userName.slice(5, 8)); // Пуп
+const fullUserName = 'Вася aka Terminator Пупкин';
+const userName = fullUserName.slice(0, userName.indexOf(' '));
+const userSurname = fullUserName.slice(userName.lastIndexOf(' ') + 1);
+console.log(name, surname);
