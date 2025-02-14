304027a4e75fc6c4122bd210d52107269947a555
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-14 17:49:20 -0500

    Преобразование строки, методы

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const fullUserName = 'Вася aka Terminator Пупкин';
-const userName = fullUserName.slice(0, userName.indexOf(' '));
-const userSurname = fullUserName.slice(userName.lastIndexOf(' ') + 1);
-console.log(name, surname);
+const str = 'Вася Пупкина';
+console.log(str.includes('а'));
+console.log(str.startsWith('В'));
+console.log(str.endsWith('н'));
+// Не меняют исходную строку
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.replace('В', 'Ф'));
+console.log(str.replace('а', 'и')); // Вися Пупкина
+console.log(str.replaceAll('а', 'и')); // Вися Пупкини
+console.log(str.replace(/а/g, 'и')); // Вися Пупкини
+
+const str2 = '     Вася Пупкина      ';
+console.log(str2);
+console.log(str2.trim()); // 'Вася Пупкина'
+console.log(str2.trimStart());
+console.log(str2.trimEnd());
+
