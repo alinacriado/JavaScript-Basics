0c0e3049884cfc1c79207d4f34654fe5d1f853eb
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-15 18:00:37 -0500

    Упражнение - проверка номера

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const str = 'Вася Пупкина';
-console.log(str.includes('а'));
-console.log(str.startsWith('В'));
-console.log(str.endsWith('н'));
-// Не меняют исходную строку
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str.replace('В', 'Ф'));
-console.log(str.replace('а', 'и')); // Вися Пупкина
-console.log(str.replaceAll('а', 'и')); // Вися Пупкини
-console.log(str.replace(/а/g, 'и')); // Вися Пупкини
 
-const str2 = '     Вася Пупкина      ';
-console.log(str2);
-console.log(str2.trim()); // 'Вася Пупкина'
-console.log(str2.trimStart());
-console.log(str2.trimEnd());
+/* проверить является ли это номером телефона России */
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = '     +7(910) 323-53-56   '
+/* не верные */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+ const num4Error = '89103g35356';
+// console.log(isNaN(Number('222'))); // false
+// console.log(isNaN(Number('bgjhgvj'))); // true
 
+function isCorrectNumber(number) {
+  number = number.trim();
+  number = number.replaceAll('(', '');
+  number = number.replaceAll(')', '');
+  number = number.replaceAll('-', '');
+  number = number.replaceAll(' ', '');
+  let numberIsCorrect = true;
+  if (number.length != 11 && number.length != 12) {
+    numberIsCorrect = false;
+  }
+  if (!number.startsWith('+7') && !number.startsWith('8')) {
+    numberIsCorrect = false;
+  }
+  for (const num of number) {
+    if (isNaN(Number(num)) && num !== '+') {
+      numberIsCorrect = false;
+      break;
+    }
+  }
+  return numberIsCorrect;
+}
+
+console.log(isCorrectNumber(num1));
+console.log(isCorrectNumber(num2));
+console.log(isCorrectNumber(num3));
+console.log(isCorrectNumber(num4));
+console.log(isCorrectNumber(num5));
+console.log('---------')
+console.log(isCorrectNumber(num1Error));
+console.log(isCorrectNumber(num2Error));
+console.log(isCorrectNumber(num3Error));
+console.log(isCorrectNumber(num4Error));
