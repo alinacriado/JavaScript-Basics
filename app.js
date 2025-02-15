b088570fd78ccbcb1ed1dc00daf727ce046a933b
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-15 18:48:42 -0500

    Обращение к элементам

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const userArray = ['Вася', 'Пупкин', 24];
+
 const user = {
   name: 'Вася',
   surname: 'Пупкин',
@@ -7,5 +7,24 @@ const user = {
     'програмирование',
     'готовка'
   ],
-  1: 1
+  educationBasic: 'Школа',
+  educationPro: 'МФТИ'
 };
+
+console.log(user.name);
+console.log(user.skills); // использовать помаксимум
+console.log(user['skills']); // для расчетеных или принимаемых
+console.log(user.skills[0]); // програмирование
+
+const level = 'Pro';
+console.log(user['education' + level]); // МФТИ
+
+const res = 'age';
+console.log(user[res]); // 24
+
+user.city = 'Москва'; // новое
+// user['city'] = 'Москва'; // новое
+console.log(user);
+
+user.age = 30; // перезаписать
+// user['age'] = 30; // перезаписать
