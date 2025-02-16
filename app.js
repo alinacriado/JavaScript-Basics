72b244f1118644de60eb085086c2df14f5e0d54c
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-15 20:13:37 -0500

    Упражнение - кошелёк

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const user = {
-  name: 'Вася',
-  surname: 'Пупкин',
-  age: 30,
-  // getFullName: function () {
-  //   return user.name + ' ' + user.surname;
-  // } не надежна при смене наименования
-  getFullName: function () {
-    console.log(this); // получим исходный объект
-    return this.name + ' ' + this.surname;
+/*
+  Релизовать методы увеличения и уменьшения баланса,
+  при котором каждая операция сохраняется в массив
+  operations в виде { reason: 'Оплата налогов', sum: -100 }.
+  Возвращается true, если успешно и false, если не хватает баланса.
+   Так же реализовать метод вывода числа операций по кошельку
+*/
+
+const wallet = {
+  balance: 0,
+  operations: [],
+  balanceIncrease: function (userReason, userSum) {
+    this.balance += userSum;
+    this.operations.push({
+      reason: userReason,
+      sum: userSum
+    });
+    return true;
+  },
+  balanceDecrease: function (userReason, userSum) {
+    if (this.balance < userSum) {
+      console.log(false);
+      return false;
+    }
+    this.balance -= userSum;
+    this.operations.push({
+      reason: userReason,
+      sum: -userSum
+    });
+    return true;
+  },
+  operationsQuantity: function () {
+    console.log(this.operations.length);
   }
 };
 
-// const getFullName = function (user) {
-//   return user.name + ' ' + user.surname;
-// };
-
-console.log(user.getFullName());
+wallet.balanceIncrease('ЗП', 1000);
+wallet.balanceDecrease('Налоги', 300);
+wallet.balanceDecrease('Купить телефон', 900);
+console.log(wallet);
+wallet.operationsQuantity();
