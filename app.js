b4f0c9dbcacc3d962d009711644281ef98dae94f
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-20 15:51:02 -0500

    Упражнение - объект в объекте

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const user = {
-  name: 'Anton',
-  id: 1,
-  roles: ['Admin']
+'use strict';
+
+const company = {
+  name: 'ООО Агро',
+  employees: [
+    {
+      name: 'Света',
+      getEmployeeName: function () {
+        return this.name;
+      }
+    }
+  ],
+  ceo: {
+    name: 'Вася',
+    getCEOName: function () {
+      return this.name;
+    }
+  },
+  getCompanyName: function () {
+    return this.name;
+  }
 };
 
-// const newUser = user;
-// newUser.name = 'newUser';
-// console.log(user); // {name: 'newUser', id: 1, roles: Array(1)}
-// console.log(newUser); // {name: 'newUser', id: 1, roles: Array(1)}
-
-const newUser2 = {
-  ...user
-};
-newUser2.name = 'newUser';
-newUser2.roles.push('user');
-console.log(user); // {name: 'Anton', id: 1, roles: Array(2)}
-console.log(newUser2); // {name: 'newUser', id: 1, roles: Array(2)}
+console.log(company.getCompanyName());
+console.log(company.ceo.getCEOName());
+for (const amployee of company.employees) {
+  console.log(amployee.getEmployeeName());
+}
