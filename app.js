7db6379b4a4ec2ce7af0234873decd74571950c4
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-20 16:16:40 -0500

    Call, apply

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 'use strict';
 
-const company = {
-  name: 'ООО Агро',
-  employees: [
-    {
-      name: 'Света',
-      getEmployeeName: function () {
-        return this.name;
-      }
-    }
-  ],
-  ceo: {
-    name: 'Вася',
-    getCEOName: function () {
-      return this.name;
-    }
-  },
-  getCompanyName: function () {
-    return this.name;
+const audi = {
+  make: 'Audi',
+  model: 'A3',
+  year: 2021,
+  damages: [],
+  addDamage(part, rate) {
+    console.log(`У авто ${this.make} поврежден ${part}`);
+    this.damages.push({
+      part,
+      rate
+    })
   }
 };
 
-console.log(company.getCompanyName());
-console.log(company.ceo.getCEOName());
-for (const amployee of company.employees) {
-  console.log(amployee.getEmployeeName());
-}
+const bmw = {
+  make: 'BMW',
+  model: 'X5',
+  year: 2022,
+  damages: [],
+};
+
+const porche = {
+  make: 'Porche',
+  model: 'tili',
+  year: 2024,
+  damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+audi.addDamage('Капот', 1);
+bmw.addDamage('Капот', 1);
+
+const addDamageFunction = audi.addDamage;
+
+addDamageFunction.call(audi, 'бампер', 2); // назначаем this и передаем аргументы
+addDamageFunction.apply(bmw, ['бампер', 2]); // // назначаем this и передаем аргументы
+
+addDamageFunction.call(porche, 'Дверь', 1);
