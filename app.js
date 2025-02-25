71815dbe43b05f5e079ad6ecc56e936ffa47bea6
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-02-25 18:53:49 -0500

    Обработка нажатий

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 'use strict';
-const panel = document.querySelector('.panel');
-console.log(panel); // <div class="panel">I love this!</div>
-const panelText = document.querySelector('.panel').innerText;
-console.log(panelText); // I love this!
-document.querySelector('.panel').innerText = 'New text';
-document.querySelector('.input').value = 'Text';
+
+// document.querySelector('.button').addEventListener('click', function() {
+//   const input = document.querySelector('.input').value;
+//   if (!input) {
+//     return;
+//   }
+//   document.querySelector('.panel').innerText = input;
+//   document.querySelector('.input').value = '';
+// });
+
+function changeClick() {
+  const input = document.querySelector('.input').value;
+  if (!input) {
+    return;
+  }
+  document.querySelector('.panel').innerText = input;
+  document.querySelector('.input').value = '';
+}
