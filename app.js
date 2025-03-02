89645ecb0aaecfdaa5b7a9eb4145c6d6f9717198
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-03-02 17:44:02 -0500

    Упражнение - получение DOM элементов

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 'use strict';
 
-function submitForm() {
-  const input = document.querySelector('.input').value;
-  if (!input) {
-    return;
-  }
-  document.querySelector('.panel').innerText = input;
-  document.querySelector('.input').value = '';
-  // document.querySelector('.notification').style.display = 'block';
-  document.querySelector('.notification').classList.add('notification_active');
-  // document.querySelector('.notification').classList.remove('notification_hidden');
-  document.querySelector('.notification').setAttribute('key', 1);
-  console.log(document.querySelector('.notification').getAttribute('class'));
-  console.log(Number(document.querySelector('.notification').getAttribute('key')));
-}
+document.querySelectorAll('.one').forEach(element => {
+  console.log(element.innerText);
+});
 
-
-function inputChanged(e) {
-  if (e.code === 'Enter') {
-    submitForm();
-  }
-};
+// console.log(document.querySelector('#two').innerText);
+console.log(document.getElementById('two').innerText);
+console.log(document.querySelector('[user-id="4"]').innerText);
