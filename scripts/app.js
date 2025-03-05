4b4478532c8878d74eda71da56f2fb0a1126fac7
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2025-03-05 15:23:17 -0500

    Загрузка и сохранение данных

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,23 @@
 'use strict';
 
+let habbits = [];
+const HABBIT_KEY = "HABBIT_KEY";
+
+/* utils */
+function loadData() {
+  const habbitsString = localStorage.getItem(HABBIT_KEY);
+  const habbitsArray = JSON.parse(habbitsString);
+  if(Array.isArray(habbitsArray)) {
+    habbits = habbitsArray;
+  }
+}
+
+function saveData() {
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+(() =>  {
+  loadData();
+})();
+
+console.log(habbits);
